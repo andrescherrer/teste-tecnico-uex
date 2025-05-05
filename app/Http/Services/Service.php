99b9b661746bb4cd58ef135cd9ddb08193e365c0
André C. Scherrer <andrescherrer@gmail.com>
2025-05-05 14:54:24 -0300

    feat: adiciona Service abstract class

---
+++ app/Http/Services/Service.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Service
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+    
+    abstract public function filter(Request $request): LengthAwarePaginator;
+
+    abstract public function create(Request $request): bool;
+
+    abstract public function findOrFail($id): Model | bool;
+
+    abstract public function update(Request $request): bool;
+}
